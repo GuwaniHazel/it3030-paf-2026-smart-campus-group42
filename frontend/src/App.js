95a6940b970c6f9472520bb13c951e4a2fec9496
Anuja Ananda <anugayaniananda@gmail.com>
2026-04-17 14:45:58 +0530

    update app navigation to include ticket pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import CreateTicket from "./pages/CreateTicket";
 import MyTickets from "./pages/MyTickets";
+import AdminTickets from "./pages/AdminTickets";
 
 function App() {
   const [page, setPage] = useState("create");
@@ -11,14 +12,15 @@ function App() {
       <button onClick={() => setPage("list")} style={{ marginLeft: "10px" }}>
         My Tickets
       </button>
+      <button onClick={() => setPage("admin")} style={{ marginLeft: "10px" }}>
+        Admin Tickets
+      </button>
 
       <hr />
 
-      {page === "create" ? (
-        <CreateTicket onSuccess={() => setPage("list")} />
-      ) : (
-        <MyTickets />
-      )}
+      {page === "create" && <CreateTicket onSuccess={() => setPage("list")} />}
+      {page === "list" && <MyTickets />}
+      {page === "admin" && <AdminTickets />}
     </div>
   );
 }
